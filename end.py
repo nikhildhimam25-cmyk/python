7a6ebea7ae00e4430e03e2a9a8111a30f325804a
nikhildhimam25-cmyk <nikhildhimam25@gmail.com>
2025-12-03 14:42:48 +0530

    done

--- end.py
+++ end.py
@@ -1,7 +1,19 @@
+
+# class bike:
+#  def get(self,a,b):
+#   # self.a=a
+#   # self.b=b
+#   print(self,a,b)
+# (bike.get('gt 650',400000,'mitra dii bike'))
+
+
 class bike:
- def start(self,a):
-  self.a=a
-  print(a,'bike is started')
-c=bike()
-c.start('splendor')
-  
+ def __init__(self,a,b):
+  self.name=a
+  self.price=b
+  print(self.name,self.price)
+#  def get(self):
+#   self.name=input('enter bike name ')
+#   self.price=input('enter bike price ')
+b1=bike('splendor',100000)
+b2=bike('triumph 440',40000)
